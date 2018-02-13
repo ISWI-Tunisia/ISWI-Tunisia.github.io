c90ee44b7850000acdab49639fddff3e47a32e04
astrax <ammarahmed.ph@gmail.com>
2018-02-13 14:22:27 +0100

    create web page

---
+++ src/makefile.sh
@@ -0,0 +1,5 @@
+
+doconce format html bootstrap_demo --html_style=bootswatch_darkly --pygments_html_style=monokai --html_admon=bootstrap_alert --html_output=index --keep_pygments_html_bg --html_code_style=inherit --html_pre_style=inherit
+ cp index.html ../
+
+doconce clean
